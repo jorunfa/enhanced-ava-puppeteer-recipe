03c629d89d62d5875d511f9aa84329fba60b3743
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2019-08-28 13:14:00 +0200

    Change example test names to reflect ts/js

--- test/hello.js
+++ test/hello.js
@@ -3,7 +3,7 @@
 import test from 'ava';
 import withPage from './helpers/_withPage';
 
-test('Example.com contains Example Domain heading', withPage, async (t, page) => {
+test('Example.com contains Example Domain heading using js', withPage, async (t, page) => {
     await page.goto('http://example.com');
     t.is(await page.$eval('h1', el => el.textContent), 'Example Domain');
 });
