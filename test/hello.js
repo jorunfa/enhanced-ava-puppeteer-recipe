3eff65a9642c02f9691c2f264b356781e9bba33c
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2019-08-28 12:56:13 +0200

    Add hello.js example

---
+++ test/hello.js
@@ -0,0 +1,10 @@
+'use strict';
+
+import test, { ExecutionContext } from 'ava';
+import withPage from './helpers/_withPage';
+import { Page } from 'puppeteer';
+
+test('Example.com contains Example Domain heading', withPage, async (t, page) => {
+    await page.goto('http://example.com');
+    t.is(await page.$eval('h1', el => el.textContent), 'Example Domain');
+});
